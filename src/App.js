1d92c56e7f294e716fec606bd1068e58ac565a53
Dimas Satria Prakoso <prakosodimassatria@gmail.com>
2022-09-29 11:27:46 +0700

    install algosdk and env

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import { useState } from 'react';
+import algosdk from 'algosdk';
 
 function App() {
-  const createTxn = () => {
-    console.log("create txn....");
-  };
-  
   const [response, setResponse] = useState("response");
+
+  const createTxn = async () => {
+    console.log("create txn....");
+
+    const token = process.env.REACT_APP_ALGOD_TOKEN;
+    const server = process.env.REACT_APP_ALGOD_ADDRESS;
+    const port = process.env.REACT_APP_ALGOD_PORT;
+    const client = new algosdk.Algodv2(token, server, port);
+
+    try {
+      const status = await client.status().do();
+      console.log(status);
+      // setResponse(await client.status().do());
+    } catch (error) {
+      console.log(error);
+      setResponse(error.message);
+    }
+  };
 
   return (
     <div className="App">
