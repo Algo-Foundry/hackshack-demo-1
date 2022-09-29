f717855aa4d48f0573fedd75668fa432d25bee67
Dimas Satria Prakoso <prakosodimassatria@gmail.com>
2022-09-29 11:51:05 +0700

    add algosdk makeApplicationNoOpTxn

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+### Setup Frontend
+npm install
+create .env file based on .env.example
+npm start
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import algosdk from 'algosdk';
 
 function App() {
-  const [response, setResponse] = useState("response");
+  const [response, setResponse] = useState("");
 
   const createTxn = async () => {
     console.log("create txn....");
@@ -14,9 +14,39 @@ function App() {
     const client = new algosdk.Algodv2(token, server, port);
 
     try {
-      const status = await client.status().do();
-      console.log(status);
-      // setResponse(await client.status().do());
+      let creatorAccount = algosdk.mnemonicToSecretKey(process.env.REACT_APP_CREATOR_MNEMONIC);
+      let sender = creatorAccount.addr;
+      let params = await client.getTransactionParams().do();
+      let index = process.env.REACT_APP_INDEX_APP_ID
+
+      // create unsigned transaction
+      let txn = algosdk.makeApplicationNoOpTxn(sender, params, index)
+      let txId = txn.txID().toString();
+
+      // sign, send, await
+      // Sign the transaction
+      let signedTxn = txn.signTxn(creatorAccount.sk);
+      console.log("Signed transaction with txID: %s", txId);
+
+      // Submit the transaction
+      await client.sendRawTransaction(signedTxn).do();
+
+      // Wait for transaction to be confirmed
+      let confirmedTxn = await algosdk.waitForConfirmation(client, txId, 4);
+      //Get the completed Transaction
+      console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+
+      // display results
+      let transactionResponse = await client.pendingTransactionInformation(txId).do();
+      console.log("Called app-id:", transactionResponse['txn']['txn']['apid'])
+      if (transactionResponse['global-state-delta'] !== undefined ) {
+          console.log("Global State updated:",transactionResponse['global-state-delta']);
+      }
+      if (transactionResponse['local-state-delta'] !== undefined ) {
+          console.log("Local State updated:",transactionResponse['local-state-delta']);
+      }
+
+      setResponse("Called app-id:", transactionResponse['txn']['txn']['apid']);
     } catch (error) {
       console.log(error);
       setResponse(error.message);
